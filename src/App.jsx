d46640e84dc84ff2db2065e09a80a902470209bf
Tri Phan <tripp@ames.edu.vn>
2023-02-03 11:30:16 +0700

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Dashboard from './scenes/dashboard'
+import Sidebar from './scenes/global/Sidebar'
+import Topbar from './scenes/global/Topbar'
 
 const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
+	return (
+		<div className='app'>
+			<Sidebar />
+			<Topbar />
+			<Dashboard />
+		</div>
+	)
 }
 
 export default App
