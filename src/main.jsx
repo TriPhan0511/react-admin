a100621453134335dbc97af437c374af234a2400
Tri Phan <phanphutri@gmail.com>
2023-02-09 20:59:57 +0700

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import UseMemoSample from './samples/useMemoSample'
-import UseCallbackSample from './samples/useCallbackSample'
-import Main from './samples/react-context-createContext-useontext/Main'
-import Application from './samples/react-context-createContext-useontext/usecase-global-user-name/Application'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
